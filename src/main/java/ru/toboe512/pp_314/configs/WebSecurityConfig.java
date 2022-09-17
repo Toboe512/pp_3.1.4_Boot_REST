9d3c80fa15df0dd43e7ada55a37f51fe4cf8749a
Toboe512 <spown@list.ru>
2022-09-17 15:30:13 +0300

    Version_1.0.0

--- src/main/java/ru/toboe512/pp_314/configs/WebSecurityConfig.java
+++ src/main/java/ru/toboe512/pp_314/configs/WebSecurityConfig.java
@@ -33,8 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors().disable()
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/admin", "/api/admin", "/js/UsersData.js").hasRole("ADMIN") //, "/js/**"
-                .antMatchers("/user").hasRole("USER") //, "/api/user"
+                .antMatchers("/admin", "/api/admin", "/js/UsersData.js").hasRole("ADMIN")
+                .antMatchers("/user").hasRole("USER")
                 .antMatchers("/api/user","/js/AuthData.js").hasAnyRole("USER", "ADMIN")
                 .antMatchers("/login", "/error").permitAll()
                 .anyRequest().hasAnyRole("USER", "ADMIN")
